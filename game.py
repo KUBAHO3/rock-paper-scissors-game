471149bc946f93c6c18af3f1ffa0a817f2a28d59
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-09-22 21:48:19 +0200

    Add validation

--- game.py
+++ game.py
@@ -1,52 +1,65 @@
 import random
 
-# print("Start ‘rock-paper-scissors’ game")
-# print("Input your hand choice")
 
-# hand = int(input('0=rock, 1=scissors, 2=paper2 \n'))
-# comp = random.randint(0, 2)
-
-# print('Your hand is', hand)
-# print('Computers hand is', comp)
-
-# if hand == comp:
-#     print('Draw')
-# elif (hand == 0 and comp == 1) or (hand == 1 and comp == 2) or (hand == 2 and comp == 0):
-#     print('win')
-# else:
-#     print('Loose')
-
-hands = ['rock', 'scissors', 'paper2']
 
 def start_message():
     print("Start ‘rock-paper-scissors’ game")
 
 def get_player():
-    hand = int(input('0=rock, 1=scissors, 2=paper2 \n'))
-    print('your hand is', hands[hand])
+    hand = input('0=rock, 1=scissors, 2=paper \n')
     return hand
 
 def get_computer():
     comp = random.randint(0, 2)
-    print('comp hand is', hands[comp])
     return comp
 
-def view_result(get_player, get_computer):
-    if get_player == get_computer:
-        print('Draw try again')
-        print("\n --------------------------\n\n")
-        initializer()
-    elif (get_player == 0 and get_computer == 1) or (get_player == 1 and get_computer == 2) or (get_player == 2 and get_computer == 0):
-        print('you win')
-        print("\n --------------------------\n\n")
+def get_hand_name(choice):
+    hands = ['rock', 'scissors', 'paper']
+    return hands[int(choice)]
+
+def view_hand(player, computer):
+    print('My hand is', get_hand_name(player))
+    print('Computer\'s hand is', get_hand_name(computer))
+
+def view_result(res):
+    results={'win':'you win', 'lose':'you lose', 'draw':'draw try again'}
+    print(results[res])
+
+def is_hand(any):
+    if any.isdigit():
+        no = int(any)
+        if no >= 0 and no <= 2:
+            return True
+        else:
+            return False
     else:
-        print('you Loose')
-        print("\n --------------------------\n\n")
+        return False
+
+
+def get_result(hands, comp):
+    hand = int(hands)
+    if hand == comp:
+        return 'draw'
+    elif (hand == 0 and comp == 1) or (hand == 1 and comp == 2) or (hand == 2 and comp == 0):
+        return 'win'
+    else:
+        return 'lose'
 
 def initializer():
-    start_message()
+    
     hand = get_player()
-    comp = get_computer()
-    view_result(hand, comp)
+    while is_hand(hand) == False:
+        print('Please enter number between 0 and 2')
+        hand = get_player()
 
+    comp = get_computer()
+
+    view_hand(hand, comp)
+
+    print(get_result(hand, comp))
+
+    if get_result(hand, comp) == 'draw':
+        initializer()
+
+start_message()
 initializer()
