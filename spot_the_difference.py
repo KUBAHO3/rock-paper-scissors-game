fb248db00bfb77512ee2e820c338b85e88666b7a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-09-28 12:15:49 +0200

    2.3 random init

--- spot_the_difference.py
+++ spot_the_difference.py
@@ -1,3 +1,5 @@
+import random
+data = [['O','0'], ['l','1'], ['u','v']]
 
 def start_message():
     print('Start')
@@ -6,8 +8,13 @@ def section_message():
     level = 1
     print('A'+ str(level))
 
+def view_question():
+    choice = random.randint(0,2)
+    print(data[choice])
+
 def play():
     section_message()
+    view_question()
 
 start_message()
 play()
