8a0cbc9c01e147e5266f3128de727425bb488109
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-09-29 10:07:40 +0200

    2.7 input cell number

--- spot_the_difference.py
+++ spot_the_difference.py
@@ -8,6 +8,17 @@ def section_message():
     level = 1
     print('A'+ str(level))
 
+def change_input_number(input_str):
+    str_dictionary = {'A': 0, 'B': 1, 'C': 2, 'D': 3}
+    input_str_split = list(input_str)
+    col_represantation = input_str_split[0]
+    row_represantation = int(input_str_split[1])
+    col_number = str_dictionary[col_represantation]
+    row_number = row_represantation - 1
+    input_number = row_number * 3 + col_number
+    return input_number
+
+
 def view_question():
     choice = random.randint(0,2)
     different_number = random.randint(0,8)
@@ -32,6 +43,8 @@ def view_question():
 def play():
     section_message()
     view_question()
+    in_string = input('Input the deifferent carracter e.g: A3 \n')
+    print(change_input_number(in_string))
 
 start_message()
 play()
