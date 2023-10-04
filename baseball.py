a3c06869162c624ca3adce712ae12906b1c6a194
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-04 15:30:37 +0200

    3.3 Display information of instance

--- baseball.py
+++ baseball.py
@@ -7,14 +7,19 @@ class Team:
         self.attack = attack
         self.defense = defense
 
+    def info(self):
+        print(self.name + ': offensive power: '+ str(self.attack) + ' / defensive power: '+ str(self.defense))
+
 def create_teams():
     global teams 
     team1 = Team('attackers', 80, 20)
     team2 = Team('deffenders', 30, 70)
     team3 = Team('Averages', 50, 50)
     teams = [team1, team2, team3]
+    team1.info()
 
 def play():
     print('Debug: play()')
+    create_teams()
 
 play()
