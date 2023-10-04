0704e51f5a78743437ff92fae6e353a58e1a7e84
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-04 15:10:49 +0200

    3.2 Create 3 instances

--- baseball.py
+++ baseball.py
@@ -7,6 +7,12 @@ class Team:
         self.attack = attack
         self.defense = defense
 
+def create_teams():
+    global teams 
+    team1 = Team('attackers', 80, 20)
+    team2 = Team('deffenders', 30, 70)
+    team3 = Team('Averages', 50, 50)
+    teams = [team1, team2, team3]
 
 def play():
     print('Debug: play()')
