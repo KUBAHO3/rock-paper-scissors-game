cabbfa33a9d6b4fab58872c42da7910abb6b2938
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-04 09:30:06 +0200

    solve challenge using while loop

--- double_loop.py
+++ double_loop.py
@@ -2,6 +2,16 @@
 array = [['January', 'February', 'March', 'April'], ['May', 'June', 'July', 'August'], ['September', 'October', 'November', 'December']]
 
 # double loop to loop into array inside other array & print month names
-for trimester in array:
-    for month in trimester:
-        print(month)
+# for trimester in array:
+#     for month in trimester:
+#         print(month)
+
+i = 0
+j = 0
+while i < len(array):
+    trimester = array[i]
+    while j < len(trimester):
+        print(trimester[j])
+        j += 1
+    i += 1
+    j = 0
