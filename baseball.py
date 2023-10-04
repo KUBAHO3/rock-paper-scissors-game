f643fae2b6094bde238bc1ce109c9e79cf39c4c7
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-04 14:51:00 +0200

    3.1 Class definition

---
+++ baseball.py
@@ -0,0 +1,14 @@
+
+
+class Team:
+
+    def __init__(self, name, attack, defense):
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+
+
+def play():
+    print('Debug: play()')
+
+play()
