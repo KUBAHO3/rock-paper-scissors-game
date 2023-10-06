8ffb9b2d192ea8aab8eeca0fdfce3d9dae321004
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-06 21:11:55 +0200

    Update the create_phone()

--- smartphones.py
+++ smartphones.py
@@ -10,20 +10,20 @@ class Phone:
     # Instance(Phones) display function by accessing constructor and prints its attributes
     def info(self):
         print('Type: ' + self.type + ' Size: ' + str(self.size) + ' Color: ' + self.color)
-# Instance creator and assign them into global variable smartphone
+# Instance creator by appending new data to an existing smartphones array
 def create_phone():
     global smartphones
-    Phone1 = Phone('Android', 60, 'red')
-    Phone2 = Phone('iPhone', 50, 'Blue')
-    Phone3 = Phone('Android', 70, 'White')
-    Phone4 = Phone('Android', 50, 'Black')
-    Phone5 = Phone('Android', 60, 'Purple')
-    Phone6 = Phone('iPhone', 60, 'Black')
-    Phone7 = Phone('Android', 50, 'Green')
-    Phone8 = Phone('iPhone', 70, 'Yellow')
-    Phone9 = Phone('iPhone', 80, 'Purple')
-    Phone10 = Phone('Android', 60, 'Yellow')
-    smartphones = [Phone1, Phone2, Phone3, Phone4, Phone5, Phone6, Phone7, Phone8, Phone9, Phone10]
+    smartphones.append(Phone('Android', 60, 'red'))
+    smartphones.append(Phone('iPhone', 50, 'Blue'))
+    smartphones.append(Phone('Android', 70, 'White'))
+    smartphones.append(Phone('Android', 50, 'Black'))
+    smartphones.append(Phone('Android', 60, 'Purple'))
+    smartphones.append(Phone('iPhone', 60, 'Black'))
+    smartphones.append(Phone('Android', 50, 'Green'))
+    smartphones.append(Phone('iPhone', 70, 'Yellow'))
+    smartphones.append(Phone('iPhone', 80, 'Purple'))
+    smartphones.append(Phone('Android', 60, 'Yellow'))
+
 # All instamce displayer by looping through smartphones array
 def show_smart_phones():
     for phone in smartphones:
