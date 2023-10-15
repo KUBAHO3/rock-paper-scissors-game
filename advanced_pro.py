191cd6617120cfd1b6eb603070f28498cbba3bcc
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-15 11:59:50 +0200

    Add resize uploaded image

--- advanced_pro.py
+++ advanced_pro.py
@@ -2,15 +2,19 @@
 # name2 ='Heaven'
 
 # print(f'{name1} and {name2} are friends')
-import numpy as np
 
 
-friends = np.array(['tom', 'jane', 'Brian', 'carol', 'jack'])
+# import numpy as np
 
-i = 0
-for friend in enumerate(friends):
-    print(f'{i+1}:{friend}')
-    i += 1
+
+# friends = np.array(['tom', 'jane', 'Brian', 'carol', 'jack'])
+
+# i = 0
+# for friend in enumerate(friends):
+#     print(f'{i+1}:{friend}')
+#     i += 1
+
+
 # import requests
 # from bs4 import BeautifulSoup
 
@@ -25,3 +29,23 @@ for friend in enumerate(friends):
 # for tag in tags:
 #     print(f'{i}:{tag.getText()}')
 #     i += 1
+
+from google.colab import files
+
+uploaded_file = files.upload()
+uploaded_file_name = next(iter(uploaded_file))
+
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread(uploaded_file_name)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+height = img.shape[0]
+width = img.shape[1]
+
+
+img = cv2.resize(img, None, fx=0.1, fy=0.1)
+img = cv2.resize(img, (width, height),interpolation=cv2.INTER_NEAREST)
+
+plt.imshow(img)
