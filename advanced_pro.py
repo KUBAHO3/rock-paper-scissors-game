26aa1835f51123837da75e689da68748734909df
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-10-15 10:39:55 +0200

    Add N-dimenstional array

--- advanced_pro.py
+++ advanced_pro.py
@@ -2,25 +2,26 @@
 # name2 ='Heaven'
 
 # print(f'{name1} and {name2} are friends')
+import numpy as np
 
-# friends = ['tom', 'jane', 'Brian']
+
+friends = np.array(['tom', 'jane', 'Brian', 'carol', 'jack'])
+
+i = 0
+for friend in enumerate(friends):
+    print(f'{i+1}:{friend}')
+    i += 1
+# import requests
+# from bs4 import BeautifulSoup
+
+# url = 'https://www.bbc.com/news/world'
+
+# response = requests.get(url)
+# element = BeautifulSoup(response.text, 'lxml')
+
+# tags = element.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
 
 # i = 1
-# for friend in friends:
-#     print(f'{i}:{friend}')
+# for tag in tags:
+#     print(f'{i}:{tag.getText()}')
 #     i += 1
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://www.bbc.com/news/world'
-
-response = requests.get(url)
-element = BeautifulSoup(response.text, 'lxml')
-
-tags = element.find_all('h3', class_='gs-c-promo-heading__title gel-pica-bold nw-o-link-split__text')
-
-i = 1
-for tag in tags:
-    print(f'{i}:{tag.getText()}')
-    i += 1
-# print(tag)
